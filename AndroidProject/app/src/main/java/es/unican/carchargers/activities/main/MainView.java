33f5b4d385d03aa52f8ab2b8bffd8c3e837fd0ef
lcr386 <126773781+lcr386@users.noreply.github.com>
2023-10-15 18:45:52 +0200

    Filtrar por potencia ventana terminado

--- AndroidProject/app/src/main/java/es/unican/carchargers/activities/main/MainView.java
+++ AndroidProject/app/src/main/java/es/unican/carchargers/activities/main/MainView.java
@@ -1,8 +1,8 @@
 package es.unican.carchargers.activities.main;
 
-
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +20,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.parceler.Parcels;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -30,6 +30,7 @@ import es.unican.carchargers.R;
 import es.unican.carchargers.activities.details.DetailsView;
 import es.unican.carchargers.activities.info.InfoActivity;
 import es.unican.carchargers.model.Charger;
+import es.unican.carchargers.repository.ICallBack;
 import es.unican.carchargers.repository.IRepository;
 
 @AndroidEntryPoint
@@ -41,15 +42,9 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
     /** presenter that controls this view */
     IMainContract.Presenter presenter;
 
-
-    //TODO Creado para gestionar la apracicion del popUp cuando seleccionas filtrar por potencia
-    private Button btnPotencia;
-
-
-
-    @SuppressLint("MissingInflatedId")
     //Para elegir filtros
     AlertDialog dialogFiltros;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -60,18 +55,6 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
         // Initialize presenter-view connection
         presenter = new MainPresenter();
         presenter.init(this);
-
-        //TODO Creado para gestionar la apracicion del popUp cuando seleccionas filtrar por potencia
-        btnPotencia = findViewById(R.id.btnPotencia);
-
-
-        //TODO Al pulsar en potencia se abriria el pop up
-        btnPotencia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainView.this, popUpPotencias.class));
-            }
-        });
 
     }
 
@@ -113,6 +96,7 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
         btnAceptar.setOnClickListener(v -> {
             // TODO guardar seleccion de filtros
             dialogFiltros.dismiss();
+            filtradoPotenciaDialog();
         });
 
         Button btnCancelar = (Button)view.findViewById(R.id.btnCancelar);
@@ -130,6 +114,54 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
         // Mostrar el AlertDialog para elegir filtros
         dialogFiltros.show();
     }
+
+    public void filtradoPotenciaDialog() {
+
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(MainView.this, R.style.AlertDialogTema);
+        builder.setTitle("Marque las casillas que más se adapten a su búsqueda:");
+        builder.setIcon(R.drawable.icono_filtro);
+
+        //True = Si pinchas fuera se cierra la ventana
+        builder.setCancelable(true);
+        String[] potencias = new String[] {
+                "2kW", "7.4kW", "22kW", "40kW", "50kW"
+        };
+        //Por defecto no estará seleccionada ninguna opción
+        final boolean[] checkItems = new boolean[] {
+                false, false, false, false, false
+        };
+        //Convierte el array de colores en una lista
+        final List<String> potenciaList = Arrays.asList(potencias);
+
+        builder.setMultiChoiceItems(potencias, checkItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                //Se verifica que hay un item seleccionado
+                checkItems[which] = isChecked;
+            }
+        });
+
+        //Al pulsar aceptar
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO llamar al algoritmo que filtra por la/s potencia/s seleccionada/s
+            }
+        });
+
+        //Al pulsar cancelar
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    };
 
     @Override
     public void init() {
@@ -162,10 +194,38 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
                 Toast.LENGTH_LONG).show();
     }
 
+    /**
+     * Crea un alertDialog que avisa de un error determinado
+     * TODO: Pasar por parametro un string que rellene el campo de setMessage con el string de parametro
+     */
+    public void showLoadErrorDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Configurar el título y el mensaje de error
+        builder.setTitle("Error");
+        builder.setMessage("Error cargando cargadores");
+
+        // Configurar un botón para cerrar el diálogo
+        builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Cerrar el diálogo si el usuario hace clic en "Salir"
+                dialog.dismiss();
+            }
+        });
+
+        // Mostrar el AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
     @Override
     public void showLoadError() {
-        Toast.makeText(this, "Error cargando cargadores", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Error cargando cargadores", Toast.LENGTH_LONG).show();
+        showLoadErrorDialog();
     }
+
 
     @Override
     public void showChargerDetails(Charger charger) {
