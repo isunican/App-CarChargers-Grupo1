5de5849e0a0152ad194c2d30dfee42d657ef5c20
Samuel <scr943@alumnos.unican.es>
2023-10-25 16:15:06 +0200

    Anhadir metodo estatico alertDialog

--- AndroidProject/app/src/main/java/es/unican/carchargers/common/Utils.java
+++ AndroidProject/app/src/main/java/es/unican/carchargers/common/Utils.java
@@ -1,5 +1,8 @@
 package es.unican.carchargers.common;
 
+import android.app.AlertDialog;
+import android.content.Context;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,5 +31,27 @@ public class Utils {
         } catch (IOException e) {}
         return null;
     }
+
+    /**
+     *  Muestra un alert dialog producido por un error conocido.
+     * @param error Descripcion del error a mostrar.
+     * @param context Siempre sera "this" para que se genere el alert dialog en la misma vista
+     *                que lo llama.
+     */
+    public static void showLoadErrorDialog(String error, Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        // Configurar el título y el mensaje de error
+        builder.setTitle("Error");
+        builder.setMessage(error);
+
+        // Configurar un botón para cerrar el diálogo
+        builder.setPositiveButton("Salir", (dialog, which) -> dialog.dismiss());
+
+        // Mostrar el AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 
 }
