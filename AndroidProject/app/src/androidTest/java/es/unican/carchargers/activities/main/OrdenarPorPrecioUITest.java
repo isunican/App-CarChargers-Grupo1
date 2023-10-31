e889d295b90a5e0b220720fb9c2f56c7126e4089
lcr386 <126773781+lcr386@users.noreply.github.com>
2023-10-31 17:12:04 +0100

    Nuevo test de interfaz sin acabar

--- AndroidProject/app/src/androidTest/java/es/unican/carchargers/activities/main/OrdenarPorPrecioUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/carchargers/activities/main/OrdenarPorPrecioUITest.java
@@ -5,6 +5,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -35,6 +36,71 @@ import es.unican.carchargers.repository.Repositories;
 @HiltAndroidTest
 @UninstallModules(RepositoriesModule.class)
 public class OrdenarPorPrecioUITest {
+
+    @Rule(order = 0)  // the Hilt rule must execute first
+    public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+
+    @Rule(order = 1)
+    public ActivityScenarioRule<MainView> activityRule = new ActivityScenarioRule(MainView.class);
+
+    // necesito el context para acceder a recursos, por ejemplo un json con datos fake
+    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+
+    // inject a fake repository that loads the data from a local json file
+    // IMPORTANT: all the tests in this class must use this repository
+    @BindValue IRepository repository = Repositories
+            .getFake(context.getResources().openRawResource(R.raw.chargers_es_100));
+
+
+    @Test
+    public void ordenDialogTest() {
+
+        // CASO VALIDO ASCENDENTE
+        // Verifica el elemento de menú ordenar
+        onView(withId(R.id.orden)).perform(click());
+
+        // Verifica que el diálogo se muestra correctamente
+        onView(withText("Marque las casillas que más se adapten a su búsqueda:")).check(matches(isDisplayed()));
+
+        // Selecciona precio y radioButton ascendente.
+        onView(withText("Precio")).perform(click());
+        onView(withText("Ascendente")).perform(click());
+
+        // Verifica que las selecciones se realizaron correctamente
+        onView(withText("Precio")).check(matches(isChecked()));
+        onView(withText("Ascendente")).check(matches(isChecked()));
+
+        // Acepta el dialog
+        onView(withText("Aceptar")).perform(click());
+
+        //FALTA COMPROBACIÓN COMO LO HAGO
+        //onView(withId(R.id.lvChargers)).check(matches()); ESTO ESTA MAL
+
+ //----------------------------------------------------------------------------//
+
+        //CASO VALIDO DESCENDENTE
+        // Verifica el elemento de menú ordenar
+        onView(withId(R.id.orden)).perform(click());
+
+        // Verifica que el diálogo se muestra correctamente
+        onView(withText("Marque las casillas que más se adapten a su búsqueda:")).check(matches(isDisplayed()));
+
+        // Selecciona precio y radioButton ascendente.
+        onView(withText("Precio")).perform(click());
+        onView(withText("Descendente")).perform(click());
+
+        // Verifica que las selecciones se realizaron correctamente
+        onView(withText("Precio")).check(matches(isChecked()));
+        onView(withText("Descendente")).check(matches(isChecked()));
+
+        // Acepta el dialog
+        onView(withText("Aceptar")).perform(click());
+
+        //FALTA COMPROBACIÓN COMO LO HAGO
+        //onView(withId(R.id.lvChargers)).check(matches()); ESTO ESTA MAL
+
+    }
 
 
 }
