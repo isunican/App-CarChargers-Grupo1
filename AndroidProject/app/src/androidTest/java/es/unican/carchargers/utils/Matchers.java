75ded6358664dd627a89e48a850d7538283a4c9e
Samuel <scr943@alumnos.unican.es>
2023-11-17 11:42:20 +0100

    merge errortest

--- AndroidProject/app/src/androidTest/java/es/unican/carchargers/utils/Matchers.java
+++ AndroidProject/app/src/androidTest/java/es/unican/carchargers/utils/Matchers.java
@@ -14,6 +14,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import es.unican.carchargers.activities.main.ChargersArrayAdapter;
 import es.unican.carchargers.model.Charger;
 
 public class Matchers {
